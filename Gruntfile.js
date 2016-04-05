b28b0e77a844567dba27ed2ade4cf1962ad28176
Balard Cedric <cedric.balard@yocto.re>
2016-04-05 10:10:41 +0400

    Initialize project as npm module

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function (grunt) {
+  // init config
+  grunt.initConfig({
+    // default package
+    pkg       : grunt.file.readJSON('package.json'),
+    // hint our app
+    yoctohint : {
+      options  : {},
+      all      : [ 'src/***', 'Gruntfile.js' ]
+    },
+    // Uglify our app
+    uglify    : {
+      options : {
+        banner  : '/* <%= pkg.name %> - <%= pkg.description %> - V<%= pkg.version %> */\n'
+      },
+      api     : {
+        files : [ {
+          expand  : true,
+          cwd     : 'src',
+          src     : '**/*.js',
+          dest    : 'dist'
+        } ]
+      }
+    },
+    // unit testing
+    mochaTest : {
+      // Test all unit test
+      all  : {
+        options : {
+          reporter : 'spec',
+        },
+        src     : [ 'test/unit/*.js' ]
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('yocto-hint');
+
+  grunt.registerTask('hint', 'yoctohint');
+  grunt.registerTask('test', 'mochaTest');
+  grunt.registerTask('build', [ 'hint', 'test', 'uglify' ]);
+  grunt.registerTask('default', 'build');
+};
